03ad0ff25ef5e21aa75eb0cca1b7c9c24ce6895c
Andrew Skowno <A.Skowno@sanbi.org.za>
2025-07-04 12:32:39 +0200

    initial

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # plot_export
-testing plot and map export for booklet and webpages 
+
+Testing plot and map export for booklet and webpages
+
+-   When you make vector outputs like pdf and eps and svg (which is best for plots and simple maps as it allows G&E team to adjust elements and text etc) the font information needs to be embedded in the plot.
+
+-   To do this one must use additional steps (not just ggsave). The package extrafont makes this possible with one or two lines of code.
+
+-   For .eps and .svg simply add "device = cairo_ps" or "device = cairo_svg".
+
+-   For pdf there is an extra step first make a special device "pdf" then use use "device = "pdf""
+
+-   For complex raster based maps use png or tiff not svg, eps or pdf (standard ggsave works fine - just give output the correct .tiff or.png or .jpeg, and set dpi = 300)
---
+++ saveplotsforbooklet.R
@@ -0,0 +1,67 @@
+
+library(nbaR)
+library(tidyverse)
+library(extrafont) # this has tools for embedding fonts in vector graphics which 
+# is required by the design team of the booklet.
+
+# this is not required for complex maps (which suit tiff format outputs)
+# for web content the extra steps 
+
+# Create dummy data to test a plot output 
+dummy_df <- tibble(
+  Biome = c("Overall","Forest", "Grassland", "Fynbos", "Karoo"),
+  CR = c(27,3, 6, 18, 0),
+  EN = c(35, 5, 12, 16, 2),
+  VU = c(49, 2, 12, 23, 3),
+  LC = c(89, 12, 25, 32, 12),
+  Total = c(183,22, 55, 89, 17)
+)
+
+test_data <-  rename(dummy_df, `Critically Endangered` = CR, 
+         `Endangered` = EN, 
+         `Vulnerable` = VU, 
+         `Least Concern` = LC )
+
+# Make Plot
+
+testplot <- nba_plot(test_data,
+                     `Biome`,
+                     2:5,
+                     CHRT = "bar",
+                     NUM = TRUE,
+                     LAB = "Percentage of ecosystem types",
+                     SAVE = NULL,
+                     SCALE_TEXT = 0.6)
+
+testplot <- testplot +
+  theme(
+    legend.position = "bottom",             # keep it at the bottom
+    legend.margin = margin(l = -45, r = -5, t = -5, b = -5)) 
+
+# to tweak position of legend to prevent it being cut off - make l more negative 
+# to shift left etc - but it is also affected by SCALE_TEXT above - there is 
+# a point where you cant fit in legend 
+
+# PDF output for Graphics team - for plots and maps but very complex maps use png
+loadfonts(device = "pdf") # this is required as device = cairo_pdf is not compatible with Corel Draw
+ggsave(
+  filename = "outputs/test.pdf",    # File name with extension for format
+  plot = testplot,
+  device = "pdf", # this embeds the fonts for GE to use (see first step of addfont)
+  width = 8, height = 6, units = "cm") 
+
+# eps output also for graphics team if need be - but not for maps
+ggsave(
+  filename = "outputs/test.eps",    # File name with extension for format
+  plot = testplot,
+  device = cairo_ps, # this embeds the fonts for GE to use 
+  width = 8, height = 6, units = "cm") 
+
+# png format for maps and web content plots and maps 
+ggsave(
+  filename = "outputs/test.png",    # File name with extension for format
+  plot = testplot,
+  dpi = 300, 
+  width = 8, height = 6, units = "cm") 
+
+
